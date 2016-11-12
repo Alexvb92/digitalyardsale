8da3af33f531f0f9b3b2213079f8c1707abe11e0
Lonnie Jones <lonnie34j@gmail.com>
2016-11-12 14:11:11 -0500

    added dummy data to migrations

--- migrations/20161111005707-insert-products.js
+++ migrations/20161111005707-insert-products.js
@@ -11,11 +11,10 @@ module.exports = {
     */
     return product.bulkCreate(
       [
-        {productname: "Quarter Pound Cheese Burger"},
-        {productname: "Big Mac"},
-        {productname: "Bacon Swiss Burger"},
-        {productname: "A1 Bacon Swiss Burger"},
-        {productname: "Turkey Burger"}
+        {productname: "Pearl Drums", price: "550", location: "Cary, NC", stock: "4", imageurl: "http://pearldrum.com/common/assets/pdf/catalog/2016-price-list.jpg"},
+        {productname: "Iron Gym Pull Up Bar", price: "29.99", location: "San Francisco, CA", stock: "32", imageurl: "http://03526f9.netsolhost.com/wp-content/uploads/2011/11/irongym.jpg"},
+        {productname: "evga sc-17", price: "2700", location: "Chapel Hill, NC", stock: "1", imageurl: "http://www.ozone3d.net/public/jegx/201601/evga_sc17_gaming_laptop_01.jpg"},
+        {productname: "Ps4 Pro", price: "399", location: ", NY", stock: "2", imageurl: "https://media.playstation.com/is/image/SCEA/playstation-4-pro-vertical-product-shot-01-us-07sep16?$TwoColumn_Image$"}
 
       ]
     )
@@ -33,11 +32,10 @@ module.exports = {
     */
     return product.destroy({ where:{productname: [
 
-      "Quarter Pound Cheese Burger",
-      "Big Mac",
-      "Bacon Swiss Burger",
-      "A1 Bacon Swiss Burger",
-      "Turkey Burger"
+      "Pearl Drums",
+      "Iron Gym Pull Up Bar",
+      "evga sc-17",
+      "Ps4 Pro"
 
       ]},
       truncate: true
